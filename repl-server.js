cee722619535465a837f27502823391481630277
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-08-04 08:03:19 -0400

    feat: :beers: Aggregate commit.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- repl-server.js
+++ repl-server.js
@@ -1,10 +1,9 @@
 /** @format */
-
-var repl = require('repl');
+var repl = require('node:repl');
 var net = require('net');
 
 net
-  .createServer(function(socket) {
+  .createServer(function (socket) {
     var r = repl.start({
       prompt: 'socket ' + socket.remoteAddress + ':' + socket.remotePort + '> ',
       input: socket,
@@ -12,7 +11,7 @@ net
       terminal: true,
       useGlobal: false,
     });
-    r.on('exit', function() {
+    r.on('exit', function () {
       socket.end();
     });
     r.context.socket = socket;
--- repl.js
+++ repl.js
@@ -8,16 +8,16 @@ const sanctuary = require("sanctuary");
 const sanctuaryDef = require("sanctuary-def");
 const jsdom = require("jsdom");
 const puppeteer = require('puppeteer');
-const qtApi = require('questrade-ts')
+const qtApi = require('questrade-ts');
 const jquery = require("jquery");
 const ramda = require("ramda");
 const lodash = require("lodash/fp");
 const { JSDOM } = jsdom;
-const path = require('path')
-const log = console.log
-const util =  require('util')
-const os =  require('os')
-const HOME =  os.homedir()
+const path = require('path');
+const log = console.log;
+const util = require('util');
+const os = require('os');
+const HOME = os.homedir();
 // replServer.setupHistory(historyPath, callback)
 
 util.inspect.defaultOptions =
@@ -32,92 +32,93 @@ util.inspect.defaultOptions =
   compact: 3,
   sorted: true,
   getters: true
-}
+};
 /**
  * myPup()
  * @param {string} myUrl
  */
-const puppetUrl = async (myUrl='https://www.google.com') => {
-   const browser = await puppeteer.launch({ headless: false });
-   const page = await browser.newPage();
-   await page.goto(myUrl);
-   const content = await page.content();
-   myRepl.context.domRaw = new JSDOM(content)
-   myRepl.context.document = myRepl.context.domRaw.window.document
-   console.dir(myRepl.context.document)
-   // await page.screenshot({ path: 'example.png' });
-   // await browser.close();
-   return 0
+const puppetUrl = async (myUrl = 'https://www.google.com') => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(myUrl);
+  const content = await page.content();
+  myRepl.context.domRaw = new JSDOM(content);
+  myRepl.context.document = myRepl.context.domRaw.window.document;
+  console.dir(myRepl.context.document);
+  // await page.screenshot({ path: 'example.png' });
+  // await browser.close();
+  return 0;
 };
 
 const myRepl = repl.start({
-   terminal: true,
-   useColors: true,
-   ignoreUndefined: false,
-   useGlobal: true,
-   prompt: "\u001b[0m\u001b[45m\u001b[30m \ue74e \u001b[0m\u001b[35m\u001b[0m  ",
+  terminal: true,
+  useColors: true,
+  ignoreUndefined: false,
+  useGlobal: true,
+  prompt: "\u001b[0m\u001b[45m\u001b[30m \ue74e \u001b[0m\u001b[35m\u001b[0m  ",
 });
 
 function initializeContext() {
-   myRepl.prompt();
+  myRepl.prompt();
 
-   // (async () => {
-   //    const browser = await puppeteer.launch();
-   //    const page = await browser.newPage();
-   //    await page.goto('https://example.com');
-   //    await page.screenshot({path: 'example.png'});
+  // (async () => {
+  //    const browser = await puppeteer.launch();
+  //    const page = await browser.newPage();
+  //    await page.goto('https://example.com');
+  //    await page.screenshot({path: 'example.png'});
 
-   //    await browser.close();
-   //  })();
+  //    await browser.close();
+  //  })();
 
-   myRepl.context.C = chalk
-   myRepl.context.D = JSDOM
-   myRepl.context.L = lodash
-   myRepl.context.P = puppeteer
-   myRepl.context.puppeteer = puppeteer
-   myRepl.context.Q = jquery
-   myRepl.context.R = ramda
-   myRepl.context.S = sanctuary
-   myRepl.context.$ = sanctuaryDef
-   myRepl.context.dirLog = console.dir
-   myRepl.context.document = ""
-   myRepl.context.domRaw = ""
-   myRepl.context.errLog = console.error
-   myRepl.context.$HOME = HOME
-   myRepl.context.info = console.info
-   myRepl.context.log = console.log;
-   myRepl.context.myPup = puppetUrl
-   myRepl.context.puppetUrl = puppetUrl
-   myRepl.context.qtApi = qtApi
-   myRepl.context.qtToken = () => readFileSync(`${process.env.HOME}/tokQT.env`, 'utf8')
-   // myRepl.context.startQtApi = () => {
-   //    return qtApi.redeemToken(myRepl.context.qtToken)
-   // }
+  myRepl.context.C = chalk;
+  myRepl.context.D = JSDOM;
+  myRepl.context.L = lodash;
+  myRepl.context.P = puppeteer;
+  myRepl.context.puppeteer = puppeteer;
+  myRepl.context.Q = jquery;
+  myRepl.context.R = ramda;
+  myRepl.context.S = sanctuary;
+  myRepl.context.$ = sanctuaryDef;
+  myRepl.context.dirLog = console.dir;
+  myRepl.context.document = "";
+  myRepl.context.domRaw = "";
+  myRepl.context.errLog = console.error;
+  myRepl.context.$HOME = HOME;
+  myRepl.context.info = console.info;
+  myRepl.context.log = console.log;
+  myRepl.context.myPup = puppetUrl;
+  myRepl.context.puppetUrl = puppetUrl;
+  myRepl.context.qtApi = qtApi;
+  myRepl.context.BEL = '\x07';
+  myRepl.context.qtToken = () => readFileSync(`${process.env.HOME}/tokQT.env`, 'utf8');
+  // myRepl.context.startQtApi = () => {
+  //    return qtApi.redeemToken(myRepl.context.qtToken)
+  // }
 
-   /**
-    * @param {number} yourNumber
-    */
-   myRepl.context.toHex = (yourNumber) => log(chalk.cyan.bold(`0x${Math.round(yourNumber).toString(16).toUpperCase()}`))
-   /**
-    * @param {string} hexString
-    */
-   myRepl.context.fromHex = (hexString) => parseInt(hexString, 16)
+  /**
+   * @param {number} yourNumber
+   */
+  myRepl.context.toHex = (yourNumber) => log(chalk.cyan.bold(`0x${Math.round(yourNumber).toString(16).toUpperCase()}`));
+  /**
+   * @param {string} hexString
+   */
+  myRepl.context.fromHex = (hexString) => parseInt(hexString, 16);
 
-   // // @ts-ignore
-   console.log(chalk.yellow.bold(`Node ${process.version}`));
-   myRepl.prompt();
-   console.log(chalk.green(`Node ${path.resolve()}`));
-   myRepl.prompt();
+  // // @ts-ignore
+  console.log(chalk.yellow.bold(`Node ${process.version}`));
+  myRepl.prompt();
+  console.log(chalk.green(`Node ${path.resolve()}`));
+  myRepl.prompt();
 }
 
 historyPath = `${HOME}/.node_repl_history`;
 
-myRepl.setupHistory(historyPath, (err,nodeRepl)=>{
-   if (err) console.error('err:',err);
+myRepl.setupHistory(historyPath, (err, nodeRepl) => {
+  if (err) console.error('err:', err);
 
-   nodeRepl.on('reset', initializeContext);
-   initializeContext()
-})
+  nodeRepl.on('reset', initializeContext);
+  initializeContext();
+});
 // myRepl
 
 
